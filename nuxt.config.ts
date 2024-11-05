b013bc5ecdf05fdbc5e2edc5c2d5c112412e90b2
CatBoxy <catboxy@staff.1111.com.tw>
2024-11-05 11:48:45 +0800

    add tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/tailwindcss"],
+  tailwindcss: {
+    // Options
+    cssPath: false, // 不引入自設 tailwind css 檔
+    configPath: "tailwind.config", // 副檔名可省略，預設為 "tailwind.config"
+    exposeConfig: true,
+  },
+});
