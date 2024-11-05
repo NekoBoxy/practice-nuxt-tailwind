6df6bcd4f8d7446bd8dbd55bca696e47437f3fc7
CatBoxy <catboxy@staff.1111.com.tw>
2024-11-05 13:49:29 +0800

    add vite setting ?

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  vite: {
+    vue: {
+      customElement: true,
+    },
+  },
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss"],
   tailwindcss: {
     // Options
     cssPath: false, // 不引入自設 tailwind css 檔
-    configPath: "tailwind.config", // 副檔名可省略，預設為 "tailwind.config"
+    // configPath: "tailwind.config", // 副檔名可省略，預設為 "tailwind.config"
     exposeConfig: true,
   },
 });
